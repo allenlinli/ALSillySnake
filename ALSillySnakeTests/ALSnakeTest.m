e73da908caa2d59bb092eac532673b846d41185d
AllenLin <allenlin@kkbox.com>
2014-05-19 16:58:19 +0800

    [Refactor] setup snake, world in ALSnakeTest. Built test 'testSnakeInit'

--- ALSillySnakeTests/ALSnakeTest.m
+++ ALSillySnakeTests/ALSnakeTest.m
@@ -11,7 +11,8 @@
 #import "ALSnakeWorld.h"
 
 @interface ALSnakeTest : XCTestCase
-
+@property (strong, nonatomic) ALSnake *snake;
+@property (strong, nonatomic) ALSnakeWorld *world;
 @end
 
 
@@ -21,32 +22,36 @@
 - (void)setUp
 {
     [super setUp];
+    
+    NSUInteger length = 3;
+    NSUInteger width = 20;
+    NSUInteger height = 20;
+    ALSnakeWorldSize worldSize = ALSnakeWorldSizeMake(width, height);
+    self.world = [[ALSnakeWorld alloc]initWithSize:worldSize];
+    self.snake = [[ALSnake alloc]initWithWorld:self.world length:length];
+    
     // Put setup code here. This method is called before the invocation of each test method in the class.
 }
 
 - (void)tearDown
 {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
+    self.snake = nil;
     [super tearDown];
 }
 
 - (void)testSnakeInit
 {
-//    NSUInteger length = 3;
-//    NSUInteger width = 20;
-//    NSUInteger height = 20;
-//    ALSnakeWorldSize worldSize = ALSnakeWorldSizeMake(width, height);
-//    ALSnake *snake = [[ALSnake alloc] initWithWorldSize:worldSize length:length];
-//    
-//
-//    for (NSUInteger i = 0; i<length; i++) {
-//        NSValue *value = snake.body[i];
-//        XCTAssertNotNil(value, @"snake.body[i] is nil");
-//        ALSnakeWorldPoint body = [snake.body[i] snakeWorldPointWithValue];
-//        XCTAssertTrue(body.x == width/2.0+i && body.y == height/2.0,@"! (body.x == width/2.0+i && body.y == height/2.0)");
-//    }
-//    
-//    XCTAssert(snake.direction == ALSnakeDirectionLeft, @"! snake.direction == ALSnakeDirectionLeft");
+    ALSnake *snake = self.snake;
+    
+    for (NSUInteger i = 0; i<self.snake.bodyLength; i++) {
+        NSValue *value = snake.body[i];
+        XCTAssertNotNil(value, @"snake.body[i] is nil");
+        ALSnakeWorldPoint body = [snake.body[i] snakeWorldPointWithValue];
+        XCTAssertTrue(body.x == self.world.size.width/2.0+i && body.y ==  self.world.size.height/2.0,@"! (body.x == width/2.0+i && body.y == height/2.0)");
+    }
+    
+    XCTAssert(snake.direction == ALSnakeDirectionLeft, @"! snake.direction == ALSnakeDirectionLeft");
 }
 
 
