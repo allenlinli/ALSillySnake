ce0710200a3f0210899e6b600d56841709d506f6
AllenLin <allenlin@kkbox.com>
2014-05-19 11:22:42 +0800

    Initial Commit

---
+++ ALSillySnake/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ALSillySnake
+//
+//  Created by allenlin on 5/19/14.
+//  Copyright (c) 2014 KKBOX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
